1d3e78a6d1f543970fb5a45ae4e7c96abe1c6390
Andrija <andrija13041998@gmail.com>
2021-12-13 14:29:46 +0100

    I created table Vaults in database, VaultRepository class in DataLayar, IVaultRepository in Shared Interfaces, model classes Vault and VaultData

---
+++ DataLayer/VaultRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Shared.Models;
+using Shared.Interfaces;
+
+namespace DataLayer
+{
+    public class VaultRepository: IVaultRepository
+    {
+    }
+}
---
+++ Shared/Interfaces/IVaultRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shared.Models;
+
+namespace Shared.Interfaces
+{
+    public interface IVaultRepository
+    {
+    }
+}
