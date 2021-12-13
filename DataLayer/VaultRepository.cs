bdce4ef99b96f22d0ec1ef8a59bb10b62f9e6163
Andrija <andrija13041998@gmail.com>
2021-12-13 16:52:43 +0100

    I created GetUserByEmailAndAuthKey method in VaultRepository class.

--- DataLayer/VaultRepository.cs
+++ DataLayer/VaultRepository.cs
@@ -11,5 +11,29 @@ namespace DataLayer
 {
     public class VaultRepository: IVaultRepository
     {
+        public User GetUserByEmailAndAuthKey(string email, string authKey)
+        {
+            User user = new User();
+            using(SqlConnection sqlConnection = new SqlConnection(Helper.GetConnectionString("PasswordManagerDB")))
+            {
+                sqlConnection.Open();
+                using(SqlCommand sqlCommand = new SqlCommand())
+                {
+                    sqlCommand.Connection = sqlConnection;
+                    sqlCommand.CommandText = "SELECT * FROM USERS WHERE EmailAddress = @email AND AuthKey = @authKey";
+                    sqlCommand.Parameters.AddWithValue("@email", email);
+                    sqlCommand.Parameters.AddWithValue("@authKey", authKey);
+                    SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+                    while(sqlDataReader.Read())
+                    {
+                        user.UserID = sqlDataReader.GetInt32(0);
+                        user.EmailAddress = sqlDataReader.GetString(1);
+                        user.AuthKey = sqlDataReader.GetString(2);
+                        user.Salt = sqlDataReader.GetString(3);
+                    }
+                }
+            }
+            return user;
+        }
     }
 }
