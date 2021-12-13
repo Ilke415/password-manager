5b7dba07703f18ceaae9009133c6e4e19118cfe9
Stefan Ilić <stefanilic952@gmail.com>
2021-12-13 13:22:29 +0100

    I created class VaultBusiness and interface IVaultBusiness.

---
+++ BusinessLayer/VaultBusiness.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shared.Models;
+using Shared.Interfaces;
+namespace BusinessLayer
+{
+    public class VaultBusiness:IVaultBusiness
+    {
+
+
+    }
+}
